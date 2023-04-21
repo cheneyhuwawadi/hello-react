e9e0a5d374b93ead974a059084dd4550e84fea34
cheneyhuwawadi <aycheneyhu@163.com>
2023-04-21 20:52:49 +0800

    Add files via upload

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	tabWidth: 2, // 缩进字节数
+	useTabs: true, // 缩进使用tab
+	semi: false, // 句尾添加分号
+	singleQuote: true, // 使用单引号代替双引号
+	arrowParens: 'avoid', //  (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
+	bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
+	jsxBracketSameLine: false, // 在jsx中把'>' 是否单独放一行
+	jsxSingleQuote: true, // 在jsx中使用单引号代替双引号
+	trailingComma: 'none'
+}
